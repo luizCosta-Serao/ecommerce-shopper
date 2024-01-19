6a2f24e59aba7cc0e26680e354487cb80cf9d63d
Luiz Serão <luiz.serao@hotmail.com>
2024-01-19 09:59:26 -0300

    Alteração do componente Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,19 +8,12 @@ const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.heroLeft}>
-        <h2>Neew Arrivals Only</h2>
-        <div>
-          <div className={styles.handIcon}>
-            <p>new</p>
-            <img src={HandIcon} alt="" />
-          </div>
-          <p>collections</p>
-          <p>for everyone</p>
-        </div>
-        <div className={styles.latestBtn}>
-          <div>Latest Collection</div>
+        <h1>Collections</h1>
+        <p>you can explore ans shop many different collection from various barands here.</p>
+        <button className={styles.latestBtn}>
+          Latest Collection
           <img src={ArrowIcon} alt="" />
-        </div>
+        </button>
       </div>
       <div className={styles.heroRight}>
         <img src={HeroImg} alt="" />
