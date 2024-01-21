4b12e4da94c1c057934c4d3162c13f2bf2fd4480
Luiz Serão <luiz.serao@hotmail.com>
2024-01-20 21:06:10 -0300

    Mudança no componente NewCollections na home

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -4,19 +4,34 @@ import newCollection from '../../assets/new_collections'
 import Item from '../Item/Item'
 
 const NewCollections = () => {
+  const [products, setProducts] = React.useState(null)
+
+  React.useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products?limit=8')
+        const json = await response.json()
+        setProducts(json)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProducts()
+  }, [])
+
+
   return (
     <section className={styles.newCollections}>
       <h1>New Collections</h1>
       <hr />
       <div className={styles.collections}>
-        {newCollection.map((item, index) => (
+        {products && products.map((product, index) => (
           <Item
             key={index}
-            id={item.id}
-            name={item.name}
-            img={item.image}
-            newPrice={item.new_price}
-            oldPrice={item.old_price}
+            id={product.id}
+            name={product.title.slice(0, 20)}
+            img={product.image}
+            newPrice={product.price.toFixed(2)}
           />
         ))}
       </div>
