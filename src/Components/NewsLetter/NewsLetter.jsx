72107620917f9cb4669e37576f7a96a584c52bae
Luiz Serão <luiz.serao@hotmail.com>
2024-01-20 21:35:19 -0300

    mudança no componente NewsLetter na página home

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import styles from './NewsLetter.module.css'
 
 const NewsLetter = () => {
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  } 
+
   return (
     <section className={styles.newsLetter}>
       <h1>Get Exclusive Offers On Your Email</h1>
       <p>Subscribe to our newletter and stay updated</p>
-      <div>
-        <input type="email" placeholder='Your Email id' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='Your Email id'
+        />
         <button>Subscribe</button>
-      </div>
+      </form>
     </section>
   )
 }
