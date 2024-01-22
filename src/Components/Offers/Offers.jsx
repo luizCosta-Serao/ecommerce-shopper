906f798de64c53667c9be5b28337530d4632002d
Luiz Serão <luiz.serao@hotmail.com>
2024-01-22 08:51:55 -0300

    inserindo navegação para jewelerys no componente Offers

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import styles from './Offers.module.css'
 import ExclusiveImage from '../../assets/exclusive_image.png'
+import { useNavigate } from 'react-router-dom'
 
 const Offers = () => {
+  const navigate = useNavigate()
+
   return (
     <section className={styles.offers}>
       <div className={styles.offersLeft}>
         <h1>Exclusive</h1>
         <p>Lustrous yet understated. The new evening wear collection exclusively offered at the reopened Giorgio Armani boutique in Los Angeles.</p>
-        <button>Check Now</button>
+        <button onClick={() => navigate('/jewelerys')}>Check Now</button>
       </div>
       <div className={styles.offersRight}>
         <img src={ExclusiveImage} alt="" />
