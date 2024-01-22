2a697c9c41d797f4ab0a03af17ec98ee028fce2a
Luiz Serão <luiz.serao@hotmail.com>
2024-01-22 08:48:58 -0300

    criando navegação com button do componente Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,14 +3,17 @@ import styles from './Hero.module.css'
 import HandIcon from '../../assets/hand_icon.png'
 import ArrowIcon from '../../assets/arrow.png'
 import HeroImg from '../../assets/hero_image.png'
+import { useNavigate } from 'react-router-dom'
 
 const Hero = () => {
+  const navigate = useNavigate()
+
   return (
     <section className={styles.hero}>
       <div className={styles.heroLeft}>
         <h1>Collections</h1>
         <p>you can explore ans shop many different collection from various barands here.</p>
-        <button className={styles.latestBtn}>
+        <button onClick={() => navigate('/womens')} className={styles.latestBtn}>
           Latest Collection
           <img src={ArrowIcon} alt="" />
         </button>
